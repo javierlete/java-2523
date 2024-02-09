a65e3b2fa725a010a2b2107d207e98c2afd374ba
java <java@DESKTOP-CRSOP9N.ipartekaula.com>
2024-02-09 09:19:31 +0100

    Inicio proyecto maven-archetype-quickstart

---
+++ MF0966/uf2214/src/test/java/com/ipartek/formacion/uf2214/AppTest.java
@@ -0,0 +1,20 @@
+package com.ipartek.formacion.uf2214;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
