76dd922d4cdf0da8791c7ca4b64f05f5772d0f2f
java <java@server.ipartekaula.com>
2024-02-19 09:12:11 +0100

    Ejemplo de GET de todos los productos

---
+++ MF0966/uf2215/src/main/java/com/ipartek/formacion/uf2215/servlets/ProductosRestServlet.java
@@ -0,0 +1,62 @@
+package com.ipartek.formacion.uf2215.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.TreeMap;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/api/v1/productos")
+public class ProductosRestServlet extends HttpServlet {
+	private static TreeMap<Long, Producto> productos = new TreeMap<>();
+
+	static {
+		productos.put(1L, new Producto(1L, "Producto1", new BigDecimal(11), LocalDate.of(2000, 1, 1)));
+		productos.put(2L, new Producto(2L, "Producto2", new BigDecimal(22), LocalDate.of(2001, 1, 1)));
+		productos.put(3L, new Producto(3L, "Producto3", new BigDecimal(33), LocalDate.of(2002, 1, 1)));
+		productos.put(4L, new Producto(4L, "Producto4", new BigDecimal(44), LocalDate.of(2003, 1, 1)));
+	}
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		String fila = """
+				{ "id": %s, "nombre": "%s", "precio": %s, "fechaDeCaducidad": "%s" }
+				""";
+		
+		out.println("[");
+		
+		for(Producto p: productos.values()) {
+			out.printf(fila, p.getId(), p.getNombre(), p.getPrecio(), p.getFechaDeCaducidad());
+			
+			if(productos.lastKey() != p.getId()) {
+				out.print(",");
+			}
+		}
+		
+		out.println("]");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
