308ef4e728df23d4dca63b08b9056128729eae01
java <java@server.ipartekaula.com>
2024-03-01 10:32:28 +0100

    Formato de DTO simplificado con Vendedor incrustado

--- MF0966/mf0966ejemplo/src/main/java/com/ipartek/formacion/mf0966ejemplo/dtos/CocheSimplificadoDTO.java
+++ MF0966/mf0966ejemplo/src/main/java/com/ipartek/formacion/mf0966ejemplo/dtos/CocheSimplificadoDTO.java
@@ -1,5 +1,7 @@
 package com.ipartek.formacion.mf0966ejemplo.dtos;
 
-public record CocheSimplificadoDTO(Long id, String matricula, String marca, String modelo, Long idVendedor, String nombreVendedor, String apellidosVendedor, String dniVendedor) {
-
+public record CocheSimplificadoDTO(Long id, String matricula, String marca, String modelo, VendedorDTO vendedor) {
+	public CocheSimplificadoDTO(Long id, String matricula, String marca, String modelo, Long idVendedor, String nombreVendedor, String apellidosVendedor, String dniVendedor) {
+		this(id, matricula, marca, modelo, new VendedorDTO(idVendedor, nombreVendedor, apellidosVendedor, dniVendedor));
+	}
 }
