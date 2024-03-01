7016878e08a573b0f7a1eed4fd681b093a39c2be
java <java@server.ipartekaula.com>
2024-03-01 10:05:18 +0100

    Nuevo tipo de DTO para información simplificada de Coche

---
+++ MF0966/mf0966ejemplo/src/main/java/com/ipartek/formacion/mf0966ejemplo/dtos/CocheSimplificadoDTO.java
@@ -0,0 +1,5 @@
+package com.ipartek.formacion.mf0966ejemplo.dtos;
+
+public record CocheSimplificadoDTO(Long id, String matricula, String marca, String modelo, Long idVendedor, String nombreVendedor, String apellidosVendedor, String dniVendedor) {
+
+}
