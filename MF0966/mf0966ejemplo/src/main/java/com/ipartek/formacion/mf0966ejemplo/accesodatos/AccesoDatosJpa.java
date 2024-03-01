a2b49097eb6076989e87aa83232f43a5d532bcd3
java <java@server.ipartekaula.com>
2024-03-01 10:04:44 +0100

    Acceso de JPA global para transacciones

---
+++ MF0966/mf0966ejemplo/src/main/java/com/ipartek/formacion/mf0966ejemplo/accesodatos/AccesoDatosJpa.java
@@ -0,0 +1,43 @@
+package com.ipartek.formacion.mf0966ejemplo.accesodatos;
+
+import java.util.function.Function;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class AccesoDatosJpa {
+	private static EntityManagerFactory entityManagerFactory = Persistence
+			.createEntityManagerFactory("com.ipartek.formacion.mf0966ejemplo.entidades");
+			
+	static {
+		AccesoDatosJpa.enTransaccion(em -> {
+			return null;
+		});
+	}
+	public static <T> T enTransaccion(Function<EntityManager, T> lambda) {
+		T resultado;
+		
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		EntityTransaction transaction = entityManager.getTransaction();
+		
+		try {
+			transaction.begin();
+			resultado = lambda.apply(entityManager);
+			transaction.commit();
+			
+			return resultado;
+		} catch (Exception e) {
+			if (transaction.isActive()) {
+				transaction.rollback();
+			}
+			throw e;
+		} finally {
+			entityManager.close();
+		}
+	}
+	
+	public static void abrirConexion() {}
+	public static void cerrarConexion() {}
+}
