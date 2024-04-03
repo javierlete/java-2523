49e2fb8c73f3ec35bb6d48da67f2543c430b5f2a
javier <javier@ipartekaula.com>
2024-04-03 11:11:41 +0200

    Conmutación de form y table

--- MF0967/uf2217/public/JAVASCRIPT/js/crud.js
+++ MF0967/uf2217/public/JAVASCRIPT/js/crud.js
@@ -6,17 +6,40 @@ const productos = [
     { id: 3, nombre: 'Producto 3', precio: 32.34 },
 ];
 
+let form, table;
+
 window.addEventListener('DOMContentLoaded', function(){
+    form = document.querySelector('form');
+    table = document.querySelector('table');
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        form.style.display = 'none';
+        table.style.display = null;
+    });
+
+    form.style.display = 'none';
+
     const tbody = document.querySelector('tbody');
 
     productos.forEach(p => {
         const tr = document.createElement('tr');
         tr.innerHTML = `<th>${p.id}</th><td>${p.nombre}</td><td>${p.precio}</td>
             <td>
-                <a href="#">Editar</a>
-                <a href="#">Borrar</a>
+                <a href="javascript:formulario()">Editar</a>
+                <a href="javascript:borrar()">Borrar</a>
             </td>`;
 
         tbody.appendChild(tr);
     });
 });
+
+function formulario() {
+    form.style.display = null;
+    table.style.display = 'none';
+}
+
+function borrar() {
+
+}
