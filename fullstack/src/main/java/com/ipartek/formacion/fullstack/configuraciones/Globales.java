9a02e3700f49e8f099b0bb47a077d3a7fbb19e47
javier <javier@ipartekaula.com>
2024-04-11 12:00:38 +0200

    Globales con DAO global de Curso

---
+++ fullstack/src/main/java/com/ipartek/formacion/fullstack/configuraciones/Globales.java
@@ -0,0 +1,10 @@
+package com.ipartek.formacion.fullstack.configuraciones;
+
+import com.ipartek.formacion.fullstack.accesodatos.DaoCurso;
+import com.ipartek.formacion.fullstack.fabricas.Fabrica;
+import com.ipartek.formacion.fullstack.fabricas.FabricaEstatica;
+
+public class Globales {
+	private static final Fabrica fabrica = new FabricaEstatica();
+	public static final DaoCurso daoCurso = fabrica.getDaoCurso();
+}
