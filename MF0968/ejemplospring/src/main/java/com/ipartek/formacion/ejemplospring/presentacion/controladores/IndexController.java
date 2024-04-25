e42ab04bda6b9fe9812abaceb68d01bf0598c308
javier <javier@ipartekaula.com>
2024-04-25 10:46:00 +0200

    Validación de formularios

--- MF0968/ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/presentacion/controladores/IndexController.java
+++ MF0968/ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/presentacion/controladores/IndexController.java
@@ -3,6 +3,7 @@ package com.ipartek.formacion.ejemplospring.presentacion.controladores;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.ipartek.formacion.ejemplospring.entidades.Producto;
 import com.ipartek.formacion.ejemplospring.servicios.AdminNegocio;
 import com.ipartek.formacion.ejemplospring.servicios.UsuarioNegocio;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/")
@@ -26,13 +29,17 @@ public class IndexController {
 		return "index";
 	}
 	
-	@GetMapping("/admin")
+	@GetMapping("admin")
 	public String admin() {
 		return "admin";
 	}
 	
-	@PostMapping("/admin")
-	public String adminPost(Producto producto) {
+	@PostMapping("admin")
+	public String adminPost(@Valid Producto producto, BindingResult bindingResult) {
+		if(bindingResult.hasErrors()) {
+			return "admin";
+		}
+		
 		adminNegocio.insertarProducto(producto);
 		return "redirect:/";
 	}
