a1e91e7caa1450ef174dedf80691435cb66fc9b8
javier <javier@ipartekaula.com>
2024-04-26 11:00:27 +0200

    Nuevas operaciones de negocio para obtener categoría con productos

--- MF0968/ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/servicios/UsuarioNegocio.java
+++ MF0968/ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/servicios/UsuarioNegocio.java
@@ -1,10 +1,11 @@
 package com.ipartek.formacion.ejemplospring.servicios;
 
+import com.ipartek.formacion.ejemplospring.dtos.CategoriaConProductosDto;
 import com.ipartek.formacion.ejemplospring.entidades.Categoria;
 import com.ipartek.formacion.ejemplospring.entidades.Producto;
 
 public interface UsuarioNegocio {
 	Iterable<Producto> obtenerProductos();
 	Iterable<Categoria> obtenerCategorias();
-	Categoria obtenerCategoriaPorId(Long id);
+	CategoriaConProductosDto obtenerCategoriaPorId(Long id);
 }
