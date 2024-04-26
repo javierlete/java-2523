b55006c988927e154cbaa1d6588805a102980682
javier <javier@ipartekaula.com>
2024-04-26 10:59:19 +0200

    Petición de Categoría con producto cartesiano de Producto

--- MF0968/ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/repositorios/CategoriaRepository.java
+++ MF0968/ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/repositorios/CategoriaRepository.java
@@ -1,9 +1,12 @@
 package com.ipartek.formacion.ejemplospring.repositorios;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.ipartek.formacion.ejemplospring.entidades.Categoria;
 
 public interface CategoriaRepository extends CrudRepository<Categoria, Long> {
-
+	@Query("from Categoria c join fetch c.productos where c.id = :id")
+	Categoria obtenerCategoriaPorIdConProductos(@Param("id") Long id);
 }
