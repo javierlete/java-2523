43ba7299cfca3a5f2d46abc80c589e25cb47f507
javier <javier@ipartekaula.com>
2024-04-26 09:19:50 +0200

    Soporte de inserción de categorías

--- MF0968/ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/servicios/AdminNegocio.java
+++ MF0968/ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/servicios/AdminNegocio.java
@@ -1,7 +1,9 @@
 package com.ipartek.formacion.ejemplospring.servicios;
 
+import com.ipartek.formacion.ejemplospring.entidades.Categoria;
 import com.ipartek.formacion.ejemplospring.entidades.Producto;
 
 public interface AdminNegocio extends UsuarioNegocio {
 	Producto insertarProducto(Producto producto);
+	Categoria insertarCategoria(Categoria categoria);
 }
--- MF0968/ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/servicios/AdminNegocioImpl.java
+++ MF0968/ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/servicios/AdminNegocioImpl.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
+import com.ipartek.formacion.ejemplospring.entidades.Categoria;
 import com.ipartek.formacion.ejemplospring.entidades.Producto;
+import com.ipartek.formacion.ejemplospring.repositorios.CategoriaRepository;
 import com.ipartek.formacion.ejemplospring.repositorios.ProductoRepository;
 
 import lombok.extern.java.Log;
@@ -15,13 +17,21 @@ import lombok.extern.java.Log;
 public class AdminNegocioImpl extends UsuarioNegocioImpl implements AdminNegocio {
 
 	@Autowired
-	private ProductoRepository repo;
+	private ProductoRepository repoProducto;
+	
+	@Autowired
+	private CategoriaRepository repoCategoria;
 	
 	@Override
 	public Producto insertarProducto(Producto producto) {
 		log.info("Insertando el producto " + producto);
 		producto.setId(null);
-		return repo.save(producto);
+		return repoProducto.save(producto);
+	}
+
+	@Override
+	public Categoria insertarCategoria(Categoria categoria) {
+		return repoCategoria.save(categoria);
 	}
 
 }
