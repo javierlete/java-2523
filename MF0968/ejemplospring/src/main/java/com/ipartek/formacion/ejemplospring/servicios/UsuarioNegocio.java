135617ba4cd931d6aa51daf008d8bf897c605859
javier <javier@ipartekaula.com>
2024-04-26 09:17:50 +0200

    Negocio con Categoria

--- MF0968/ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/servicios/UsuarioNegocio.java
+++ MF0968/ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/servicios/UsuarioNegocio.java
@@ -1,7 +1,10 @@
 package com.ipartek.formacion.ejemplospring.servicios;
 
+import com.ipartek.formacion.ejemplospring.entidades.Categoria;
 import com.ipartek.formacion.ejemplospring.entidades.Producto;
 
 public interface UsuarioNegocio {
 	Iterable<Producto> obtenerProductos();
+	Iterable<Categoria> obtenerCategorias();
+	Categoria obtenerCategoriaPorId(Long id);
 }
--- MF0968/ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/servicios/UsuarioNegocioImpl.java
+++ MF0968/ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/servicios/UsuarioNegocioImpl.java
@@ -3,18 +3,32 @@ package com.ipartek.formacion.ejemplospring.servicios;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ipartek.formacion.ejemplospring.entidades.Categoria;
 import com.ipartek.formacion.ejemplospring.entidades.Producto;
+import com.ipartek.formacion.ejemplospring.repositorios.CategoriaRepository;
 import com.ipartek.formacion.ejemplospring.repositorios.ProductoRepository;
 
 @Service
 public class UsuarioNegocioImpl implements UsuarioNegocio {
 
 	@Autowired
-	private ProductoRepository repo;
+	private ProductoRepository repoProducto;
+	
+	@Autowired
+	private CategoriaRepository repoCategoria;
 	
 	@Override
 	public Iterable<Producto> obtenerProductos() {
-		return repo.findAll();
+		return repoProducto.findAll();
 	}
 
+	@Override
+	public Iterable<Categoria> obtenerCategorias() {
+		return repoCategoria.findAll();
+	}
+
+	@Override
+	public Categoria obtenerCategoriaPorId(Long id) {
+		return repoCategoria.findById(id).orElse(null);
+	}
 }
