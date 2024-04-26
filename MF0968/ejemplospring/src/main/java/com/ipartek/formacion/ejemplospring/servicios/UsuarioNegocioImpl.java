8fccea8a1b12f27f29945f5b29f9f392e5119e5c
javier <javier@ipartekaula.com>
2024-04-26 11:00:38 +0200

    Implementación con tres opciones

--- MF0968/ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/servicios/UsuarioNegocioImpl.java
+++ MF0968/ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/servicios/UsuarioNegocioImpl.java
@@ -1,8 +1,12 @@
 package com.ipartek.formacion.ejemplospring.servicios;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ipartek.formacion.ejemplospring.dtos.CategoriaConProductosDto;
+import com.ipartek.formacion.ejemplospring.dtos.ProductoDto;
 import com.ipartek.formacion.ejemplospring.entidades.Categoria;
 import com.ipartek.formacion.ejemplospring.entidades.Producto;
 import com.ipartek.formacion.ejemplospring.repositorios.CategoriaRepository;
@@ -13,10 +17,10 @@ public class UsuarioNegocioImpl implements UsuarioNegocio {
 
 	@Autowired
 	private ProductoRepository repoProducto;
-	
+
 	@Autowired
 	private CategoriaRepository repoCategoria;
-	
+
 	@Override
 	public Iterable<Producto> obtenerProductos() {
 		return repoProducto.findAll();
@@ -28,7 +32,20 @@ public class UsuarioNegocioImpl implements UsuarioNegocio {
 	}
 
 	@Override
-	public Categoria obtenerCategoriaPorId(Long id) {
-		return repoCategoria.findById(id).orElse(null);
+	public CategoriaConProductosDto obtenerCategoriaPorId(Long id) {
+		// SOLUCIÓN CON LAZY (hay que desactivar en configuración la línea spring.jpa.open-in-view=false o ponerle el valor true
+		// return repoCategoria.findById(id).orElse(null);
+
+		// SOLUCIÓN CON PRODUCTO CARTESIANO
+		// return repoCategoria.obtenerCategoriaPorIdConProductos(id);
+
+		// SOLUCIÓN CON "N+1" O MÁS BIEN DOS CONSULTAS A TRAVÉS DE DTOs
+		Categoria c = repoCategoria.findById(id).orElse(null);
+		Set<ProductoDto> productos = repoProducto.findByCategoriaId(id);
+
+		CategoriaConProductosDto categoriaDto = new CategoriaConProductosDto(c.getId(), c.getCodigo(), c.getNombre(),
+				c.getDescripcion(), productos);
+
+		return categoriaDto;
 	}
 }
