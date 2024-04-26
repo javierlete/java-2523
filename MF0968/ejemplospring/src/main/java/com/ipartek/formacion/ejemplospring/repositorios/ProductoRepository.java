39edadaba62603aa85da01a65e14fd2303d9832e
javier <javier@ipartekaula.com>
2024-04-26 10:59:57 +0200

    Obtener los productos de una categoría y cambio a Set

--- MF0968/ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/repositorios/ProductoRepository.java
+++ MF0968/ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/repositorios/ProductoRepository.java
@@ -1,13 +1,17 @@
 package com.ipartek.formacion.ejemplospring.repositorios;
 
 import java.math.BigDecimal;
+import java.util.Set;
 
 import org.springframework.data.repository.CrudRepository;
 
+import com.ipartek.formacion.ejemplospring.dtos.ProductoDto;
 import com.ipartek.formacion.ejemplospring.entidades.Producto;
 
 public interface ProductoRepository extends CrudRepository<Producto, Long> {
-	Iterable<Producto> findByNombreContains(String nombre);
+	Set<Producto> findByNombreContains(String nombre);
 	
-	Iterable<Producto> findByPrecioBetween(BigDecimal minimo, BigDecimal maximo);
+	Set<Producto> findByPrecioBetween(BigDecimal minimo, BigDecimal maximo);
+	
+	Set<ProductoDto> findByCategoriaId(Long id);
 }
