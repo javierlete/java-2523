9fe7518528829abbac4c8a7dd3d3e78d2134d71f
javier <javier@ipartekaula.com>
2024-04-29 08:54:56 +0200

    Spring: DevTools, Lombok, Data JPA, MySQL, Web, Validation, Thymeleaf

---
+++ MF0968/recetas/src/test/java/com/ipartek/formacion/recetas/RecetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.recetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
