88e79c748ecdbae07266b110af27347c9f047ef7
javier <javier@ipartekaula.com>
2024-04-30 09:08:23 +0200

    Autenticación y autorización básica

---
+++ MF0968/recetas/src/main/java/com/ipartek/formacion/recetas/configuraciones/WebSecurityConfig.java
@@ -0,0 +1,56 @@
+package com.ipartek.formacion.recetas.configuraciones;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+	// AUTENTICACIÓN
+	@Bean
+	UserDetailsService userDetailsService() {
+		UserDetails javier = 
+				User
+					.withDefaultPasswordEncoder()
+					.username("javier")
+					.password("lete")
+					.roles("ADMIN")
+				.build();
+		
+		UserDetails pepe = 
+				User
+					.withDefaultPasswordEncoder()
+					.username("pepe")
+					.password("perez")
+					.roles("USER")
+				.build();
+		
+
+		return new InMemoryUserDetailsManager(javier, pepe);
+	}
+
+	// AUTORIZACIÓN
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(
+				(requests) -> requests
+					.requestMatchers("/ingrediente").hasRole("ADMIN")
+					.anyRequest().authenticated()
+				)
+				.formLogin((form) -> form
+//						.loginPage("/login")
+						.permitAll()
+				)
+//				.logout((logout) -> logout.permitAll())
+				;
+
+		return http.build();
+	}
+}
