ed0a9b8aa47c02dd01ee3e599e61e387fef55e41
javier <javier@ipartekaula.com>
2024-05-02 11:19:15 +0200

    Más datos de prueba

--- MF0968/recetas/src/main/java/com/ipartek/formacion/recetas/servicios/RecetaServiceImpl.java
+++ MF0968/recetas/src/main/java/com/ipartek/formacion/recetas/servicios/RecetaServiceImpl.java
@@ -1,6 +1,7 @@
 package com.ipartek.formacion.recetas.servicios;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.math.BigDecimal;
+
 import org.springframework.stereotype.Service;
 
 import com.ipartek.formacion.recetas.entidades.Dificultad;
@@ -18,7 +19,7 @@ import com.ipartek.formacion.recetas.repositorios.UsuarioRepository;
 
 @Service
 public class RecetaServiceImpl implements RecetaService {
-	@Autowired
+
 	private PlatoIngredienteRepository platoIngredienteRepository;
 	
 	private IngredienteRepository ingredienteRepository;
@@ -29,7 +30,7 @@ public class RecetaServiceImpl implements RecetaService {
 	
 	private TipoCocinaRepository tipoCocinaRepository;
 	
-	public RecetaServiceImpl(UsuarioRepository usuarioRepository, PlatoRepository platoRepository, IngredienteRepository ingredienteRepository, DificultadRepository dificultadRepository, TipoCocinaRepository tipoCocinaRepository) {
+	public RecetaServiceImpl(PlatoIngredienteRepository platoIngredienteRepository, UsuarioRepository usuarioRepository, PlatoRepository platoRepository, IngredienteRepository ingredienteRepository, DificultadRepository dificultadRepository, TipoCocinaRepository tipoCocinaRepository) {
 		dificultadRepository.save(Dificultad.builder().nombre("Baja").puntuacion(3).build());
 		dificultadRepository.save(Dificultad.builder().nombre("Media").puntuacion(5).build());
 		dificultadRepository.save(Dificultad.builder().nombre("Alta").puntuacion(7).build());
@@ -39,15 +40,22 @@ public class RecetaServiceImpl implements RecetaService {
 		tipoCocinaRepository.save(TipoCocina.builder().nombre("Francesa").build());
 		tipoCocinaRepository.save(TipoCocina.builder().nombre("Italiana").build());
 		
+		var tomate = Ingrediente.builder().nombre("Tomate").build();
+		var jamon = Ingrediente.builder().nombre("Jamón").build();
+		
 		ingredienteRepository.save(Ingrediente.builder().nombre("Patata").build());
 		ingredienteRepository.save(Ingrediente.builder().nombre("Lechuga").build());
-		ingredienteRepository.save(Ingrediente.builder().nombre("Tomate").build());
+		ingredienteRepository.save(tomate);
+		ingredienteRepository.save(jamon);
 		
 		var tipoCocina = TipoCocina.builder().id(4L).build();
 		var dificultad = Dificultad.builder().id(1L).build();
-		var plato = Plato.builder().nombre("Pizza").preparacion("Horno").dificultad(dificultad).tipoCocina(tipoCocina).build();
+		var plato = Plato.builder().nombre("Pizza").preparacion("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum dignissimos vero eligendi beatae adipisci non necessitatibus tempore consequuntur soluta asperiores ipsa eaque similique quia! Architecto consequatur a totam aut fuga!").dificultad(dificultad).tipoCocina(tipoCocina).build();
 
 		platoRepository.save(plato);
+		
+		platoIngredienteRepository.save(PlatoIngrediente.builder().plato(plato).ingrediente(tomate).medida("rodajas").cantidad(new BigDecimal(5)).build());
+		platoIngredienteRepository.save(PlatoIngrediente.builder().plato(plato).ingrediente(jamon).medida("láminas").cantidad(new BigDecimal(20)).build());
 		
 		usuarioRepository.save(Usuario.builder().nombre("Javier").email("javier@email.net").password("$2a$12$mof.u/4EIo58hR7On9DnPevyqBC7kb9FHzT.LN/BjF8xOqQVTP1NO").rol("ADMIN").build());
 		usuarioRepository.save(Usuario.builder().nombre("Pepe").email("pepe@email.net").password("$2a$12$Dij9cgV3mXDQYtOo4nvQTOLaUz3URoe7DGjhBrqGa1fEEzkNVhBgq").rol("USER").build());
@@ -56,6 +64,7 @@ public class RecetaServiceImpl implements RecetaService {
 		this.ingredienteRepository = ingredienteRepository;
 		this.dificultadRepository = dificultadRepository;
 		this.tipoCocinaRepository = tipoCocinaRepository;
+		this.platoIngredienteRepository = platoIngredienteRepository;
 	}
 	
 	@Override
