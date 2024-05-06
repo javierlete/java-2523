a2d3b16048f1363316514d8290d97693cad6f643
javier <javier@ipartekaula.com>
2024-05-06 09:42:55 +0200

    JpaRepository también para Valoracion

--- MF0968/recetas/src/main/java/com/ipartek/formacion/recetas/repositorios/ValoracionRepository.java
+++ MF0968/recetas/src/main/java/com/ipartek/formacion/recetas/repositorios/ValoracionRepository.java
@@ -1,11 +1,11 @@
 package com.ipartek.formacion.recetas.repositorios;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.ipartek.formacion.recetas.entidades.Valoracion;
 
 @RepositoryRestResource(path = "valoraciones", collectionResourceRel = "valoraciones")
-public interface ValoracionRepository extends CrudRepository<Valoracion, Long> {
+public interface ValoracionRepository extends JpaRepository<Valoracion, Long> {
 
 }
